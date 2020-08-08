00a518e35d7f9425cae258ebc90ac96a92101274
juacrumar <juacrumar@gmail.com>
2020-08-08 13:56:37 +0200

    set priority flexible

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -26,6 +26,10 @@ setup_conda_rc "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
 source run_conda_forge_build_setup
 
+# set channel priority not strict
+# tensorflow in conda-forge is 1.14, so strict channel priority breaks everything...
+conda config --set channel_priority flexible
+
 # make the build number clobber
 make_build_number "${FEEDSTOCK_ROOT}" "${RECIPE_ROOT}" "${CONFIG_FILE}"
 
